f6fa187f66f20489159f492f77ad4f5521d3d4f3
jinhoLee <kswlsgh@naver.com>
2023-05-06 03:47:55 +0900

    q2 complete

--- AlgorithmCodingTest/Q1.java
+++
@@ -1,7 +0,0 @@
-package AlgorithmCodingTest;
-
-public class Q1 {
-    public static void main(String[] args) {
-        System.out.println("아아 테스트 테스트");
-    }
-}
---
+++ AlgorithmCodingTest/Question_1_10/Q1.java
@@ -0,0 +1,24 @@
+package AlgorithmCodingTest.Question_1_10;
+
+import java.util.Scanner;
+
+/**
+ * 숫자의 합 구하기
+ *
+ * N개의 숫자가 공백 없이 써 있다. 이숫자를 모두 합해 출력하는 프로그램을 작성하시오.
+ */
+public class Q1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int length = sc.nextInt();
+
+        String sNum = sc.next();
+        char[] cNum = sNum.toCharArray();   //문자열 sNum -> Char형 배열로 변환
+        int sum =0;
+        for (int i = 0; i < cNum.length; i++) {
+            sum += cNum[i] -'0';        //문자열에서 '0' 을 빼주어 숫자로 만든 뒤 합계산
+        }
+        System.out.println(sum);
+
+    }
+}
