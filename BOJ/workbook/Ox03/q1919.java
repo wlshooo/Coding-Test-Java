3fcafd4670a91db09c3cefa1d110772ba50af3ce
wlshooo <kswlsgh@naver.com>
2023-05-16 16:31:11 +0900

    Ox03 1919

---
+++ BOJ/workbook/Ox03/q1919.java
@@ -0,0 +1,26 @@
+package BOJ.workbook.Ox03;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class q1919 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] first = br.readLine().toCharArray();
+        char[] second = br.readLine().toCharArray();
+        int count =0;
+           int[] Falpha = new int[26];
+           int[] Salpha = new int[26];
+           for(char ch:first) Falpha[ch-97]++;
+           for(char ch:second) Salpha[ch-97]++;
+            for (int i = 0; i < Falpha.length; i++) {
+                if (Falpha[i]>Salpha[i]) {
+                    count=count+Falpha[i]-Salpha[i];
+                } else if (Falpha[i]<Salpha[i]) {
+                    count=count+Salpha[i]-Falpha[i];
+                }
+            }
+        System.out.println(count);
+    }
+}
