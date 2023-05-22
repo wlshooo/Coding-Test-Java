9adb620ab3fd75dbeaae7f7e04c9ade90b26f068
wlshooo <kswlsgh@naver.com>
2023-05-22 13:25:42 +0900

    나머지 합 구하기

--- BOJ/AlgorithmBook/Question_1_10/Q5.java
+++
@@ -1,4 +0,0 @@
-package BOJ.AlgorithmBook.Question_1_10;
-
-public class Q5 {
-}
---
+++ BOJ/AlgorithmBook/Question_1_10/Q5_10986__.java
@@ -0,0 +1,34 @@
+package BOJ.AlgorithmBook.Question_1_10;
+
+import java.util.Scanner;
+
+//나머지 합 구하기
+public class Q5_10986__ {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M  = sc.nextInt();
+        long[] S = new long[N];
+        long[] C = new long[M];
+
+        long answer = 0;
+        S[0] = sc.nextInt();
+        for (int i = 1; i < N; i++) { //수열 합 배열 만들기
+            S[i] = S[i-1] +sc.nextInt();
+        }
+        for (int i = 0; i < N; i++) {
+            int remainder = (int) (S[i]%M);
+            //0~i까지의 구간 합 자ㅔㅊ가 0일 때 정답에 더하기
+            if(remainder==0) answer++;
+            //나머지가 같은 인덱스의 개수 카운팅 하기
+            C[remainder]++;
+        }
+        for (int i = 0; i < M; i++) {
+            if (C[i] > 1) {
+                //나머지가 같은 인덱스 중 2개를 뽑는 경우의 수를 더하기
+                answer = answer +(C[i]*(C[i]-1)/2);
+            }
+        }
+        System.out.println(answer);
+    }
+}
