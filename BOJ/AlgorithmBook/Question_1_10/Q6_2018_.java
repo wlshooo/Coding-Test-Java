36f6df31fd6cf646cef48b304f917a339c2a0eec
wlshooo <kswlsgh@naver.com>
2023-05-23 11:32:14 +0900

    Q6_2018 투  포인터-연속된 자연수의 합 구하기

---
+++ BOJ/AlgorithmBook/Question_1_10/Q6_2018_.java
@@ -0,0 +1,32 @@
+package BOJ.AlgorithmBook.Question_1_10;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//투 포인터 - 연속된 자연수의 합 구하기
+public class Q6_2018_ {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int count =1;
+        int start_index =1;
+        int end_index=1;
+        int sum=1;
+
+        while (end_index != N) {
+            if (sum == N) {
+                count++;
+                end_index++;
+                sum=sum+end_index;
+            } else if (sum > N) {
+                sum=sum-start_index;
+                start_index++;
+            } else if (sum<N) {
+                end_index++;
+                sum=sum+end_index;
+            }
+        }
+        System.out.println(count);
+    }
+}
