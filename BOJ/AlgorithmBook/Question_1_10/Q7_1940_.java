310d05b35880b85e663910990062ba3cd0040f93
wlshooo <kswlsgh@naver.com>
2023-05-23 11:54:21 +0900

    Q7_1940 주몽의 망령

---
+++ BOJ/AlgorithmBook/Question_1_10/Q7_1940_.java
@@ -0,0 +1,36 @@
+package BOJ.AlgorithmBook.Question_1_10;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+//주몽의 망령
+public class Q7_1940_ {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine()); //재료의 개수
+        int M = Integer.parseInt(br.readLine()); //갑옷이 완성되는 번호의 합
+        String[] s = br.readLine().split(" ");
+        int count =0;
+        int [] A = new int[N];
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.valueOf(s[i]);
+        }
+        Arrays.sort(A); //오름차순 정렬
+        int i=0;
+        int j=N-1;
+        while (i < j) {
+            if (A[i] + A[j] == M) {
+                i++;
+                j--;
+                count++;
+            } else if (A[i] + A[j] > M) {
+                j--;
+            } else if (A[i]+A[j]<M) {
+                i++;
+            }
+        }
+        System.out.println(count);
+    }
+}
