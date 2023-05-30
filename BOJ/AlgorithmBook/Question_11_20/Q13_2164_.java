20bcce2d5fa81f7a78e3655394694fef4e237407
wlshooo <kswlsgh@naver.com>
2023-05-30 13:13:41 +0900

    Q13 2164 카드2

---
+++ BOJ/AlgorithmBook/Question_11_20/Q13_2164_.java
@@ -0,0 +1,26 @@
+package BOJ.AlgorithmBook.Question_11_20;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+//카드2
+public class Q13_2164_ {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 1; i <= N; i++) {
+            queue.add(i);
+        }
+
+        while (queue.size() > 1) {
+            queue.poll();
+            queue.add(queue.poll());
+        }
+        System.out.println(queue.poll());
+    }
+}
