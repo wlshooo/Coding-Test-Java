42e4191bb55676a40e61c81fb50f102beef4d57c
wlshooo <kswlsgh@naver.com>
2023-06-13 12:28:23 +0900

    Q36 1541 잃어버린 괄호

---
+++ BOJ/AlgorithmBook/Greedy/Q36_1541_.java
@@ -0,0 +1,31 @@
+package BOJ.AlgorithmBook.Greedy;
+
+import java.util.Scanner;
+
+//잃어버린 괄호
+public class Q36_1541_ {
+    static int answer = 0;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String example = sc.nextLine();
+        String[] str = example.split("-");
+        for (int i = 0; i < str.length; i++) {
+            int temp = mySum(str[i]);
+            if (i == 0) {
+                answer = answer + temp;
+            } else {
+                answer= answer-temp;
+            }
+        }
+        System.out.println(answer);
+    }
+
+    static int mySum(String a) {
+        int sum = 0;
+        String temp[] = a.split("[+]");
+        for (int i = 0; i < temp.length; i++) {
+            sum += Integer.parseInt(temp[i]);
+        }
+        return sum;
+    }
+}
