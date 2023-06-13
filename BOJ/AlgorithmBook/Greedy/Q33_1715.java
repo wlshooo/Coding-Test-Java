ef1ee884f9547cbe9fa95bf7448834ca335aadf1
wlshooo <kswlsgh@naver.com>
2023-06-13 11:50:41 +0900

    Q33 1715 카드정렬하기

--- BOJ/AlgorithmBook/Greedy/Q33_1715.java
+++
@@ -1,8 +0,0 @@
-package BOJ.AlgorithmBook.Greedy;
-
-//카드 정렬하기
-public class Q33_1715 {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ BOJ/AlgorithmBook/Greedy/Q33_1715_.java
@@ -0,0 +1,28 @@
+package BOJ.AlgorithmBook.Greedy;
+
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+//카드 정렬하기
+public class Q33_1715_ {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i < N; i++) {
+            int data = sc.nextInt();
+            pq.add(data);
+        }
+        int data1 = 0;
+        int data2 = 0;
+        int sum = 0;
+
+        while (pq.size() != 1) {
+            data1 = pq.remove();
+            data2 = pq.remove();
+            sum+=data1+data2;
+            pq.add(data1+data2);
+        }
+        System.out.println(sum);
+    }
+}
