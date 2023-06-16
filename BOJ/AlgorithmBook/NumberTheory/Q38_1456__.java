ad50bd723252c9d5c4e674315c4df74e062634d1
jinhoLee <kswlsgh@naver.com>
2023-06-16 18:36:02 +0900

    Q38 1456 거의소수

---
+++ BOJ/AlgorithmBook/NumberTheory/Q38_1456__.java
@@ -0,0 +1,49 @@
+package BOJ.AlgorithmBook.NumberTheory;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+//나머지 합 구하기 - 거의 소수
+public class Q38_1456__ {
+    public static int solution(long a, long b) {
+        // 에라토스테네스의 체
+        long[] arr = new long[10000001];
+        for (int i = 2; i < arr.length; i++) {
+            arr[i] = i;
+        }
+
+        for (int i = 2; i <= Math.sqrt(arr.length); i++) { // 제곱근 까지만 수행
+            if (arr[i] == 0) {
+                continue;
+            }
+            for (int j = i + i; j < arr.length; j = j + i) { // 배수 지우기
+                arr[j] = 0;
+            }
+        }
+
+        // 거의소수
+        int count = 0;
+        for (int i = 2; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                long temp = arr[i];
+                while ((double) arr[i] <= (double) b / (double) temp) {
+                    if ((double) arr[i] >= (double) a / (double) temp) {
+                        count++;
+                    }
+                    temp = temp * arr[i];
+                }
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            long a = Long.parseLong(st.nextToken());
+            long b = Long.parseLong(st.nextToken());
+            System.out.println(solution(a, b));
+    }
+}
