ca2e082649bb5dd9873fcb3f748f0dbfb64df0f8
wlshooo <kswlsgh@naver.com>
2023-06-20 11:12:25 +0900

    Q43 1850 최대공약수 구하기

---
+++ BOJ/AlgorithmBook/NumberTheory/Q43_1850.java
@@ -0,0 +1,32 @@
+package BOJ.AlgorithmBook.NumberTheory;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+
+//최대공약수 구하기
+public class Q43_1850 {
+    public static void main(String[] args) throws IOException {
+        Scanner sc =new Scanner(System.in);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        long result = gcd(a,b);
+        while (result > 0) {
+            bw.write("1");
+            result--;
+        }
+        bw.flush();
+        bw.close();
+    }
+
+    private static long gcd(long a, long b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return gcd(b,a%b);
+        }
+    }
+
+}
