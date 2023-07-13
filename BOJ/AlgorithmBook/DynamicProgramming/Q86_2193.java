693fa69428c90da940d54ca1a77c60234f24d5e3
wlshooo <kswlsgh@naver.com>
2023-07-13 10:53:38 +0900

    Q86 2193 이친수

---
+++ BOJ/AlgorithmBook/DynamicProgramming/Q86_2193.java
@@ -0,0 +1,19 @@
+package BOJ.AlgorithmBook.DynamicProgramming;
+
+import java.util.Scanner;
+
+//이친수 구하기
+public class Q86_2193 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N= sc.nextInt();
+        long D [][] = new long[N+1][2];
+        D[1][1] = 1;
+        D[1][0] = 0;
+        for (int i = 2; i <= N; i++) {
+            D[i][0] = D[i-1][1] + D[i-1][0];
+            D[i][1] = D[i-1][0];
+        }
+        System.out.println(D[N][0] + D[N][1]);
+    }
+}
