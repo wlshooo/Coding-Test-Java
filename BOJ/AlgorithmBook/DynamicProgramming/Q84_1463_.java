95dbc36e34398fe53641590d0a35faf00cf27e22
wlshooo <kswlsgh@naver.com>
2023-07-13 10:22:56 +0900

    Q84 1463 정수를 1로 만들기

---
+++ BOJ/AlgorithmBook/DynamicProgramming/Q84_1463_.java
@@ -0,0 +1,21 @@
+package BOJ.AlgorithmBook.DynamicProgramming;
+
+import java.util.Scanner;
+
+//정수를 1로 만들기
+public class Q84_1463_ {
+    static int N;
+    static int D[];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        D = new int[N+1];
+        D[1] = 0;
+        for (int i = 2; i <= N; i++) {
+            D[i] = D[i-1] +1;
+            if(i%2 == 0) D[i] = Math.min(D[i],D[i/2]+1);
+            if(i%3 == 0) D[i] = Math.min(D[i],D[i/3]+1);
+        }
+        System.out.println(D[N]);
+    }
+}
